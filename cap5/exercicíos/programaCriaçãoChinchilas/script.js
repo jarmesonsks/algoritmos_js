2ce286e82080c9a10447c41c6d31dfe9f1138866
jarmeson_eng <jarmesonsks@gmail.com>
2026-05-24 15:37:04 -0300

    Chinchilas

--- cap5/exercicíos/programaCriaçãoChinchilas/script.js
+++ cap5/exercicíos/programaCriaçãoChinchilas/script.js
@@ -3,16 +3,18 @@ const resp = document.querySelector("pre")
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
+
     const chinchilas = Number(frm.inNumero.value)
     const anos = Number(frm.inAnos.value)
+
     let resposta = "" // variavel string para concatenar a resposta
+    let total = chinchilas
 
     for (let i = 1; i <= anos; i++) {
 
-        // triplica a quantidade de chinchilas a cada ano
-        let total = chinchilas * 3 ** i
-
         resposta = `${resposta}${i}º Ano: ${total} Chinchilas\n`
+
+        total = total * 3
     }
     resp.innerText = resposta
 })
