d41f47d01769154e27c936b9983948bcf609bd44
jarmeson_eng <jarmesonsks@gmail.com>
2026-07-05 15:35:25 -0300

    fim: descubra o numero

--- cap6/js/ex6_2.js
+++ cap6/js/ex6_2.js
@@ -1,7 +1,7 @@
 const frm = document.querySelector("form")
 const respErros = document.querySelector("#outErros")
 const respChances = document.querySelector("#outChances")
-const respDica = document.querySelector("#outDica")
+const respDica = document.querySelector("#outDicas")
 
 const erros = [] // vetor global com numeros já apostados
 const sorteado = Math.floor(Math.random() * 100 + 1) // número aleatorio entre 1 e 100
@@ -11,7 +11,7 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const numero = Number(frm.inNumero.value) // obtém numero digitado
     if (numero === sorteado) { // se acertou
-        respDica.innerText = `Parabéns!! Número sorteado: ${sorteado}` 
+        respDica.innerText = `Parabéns!! Número sorteado: ${sorteado}`
         frm.btSubmit.disabled = true // troca status dos botões
         frm.btNovo.className = "exibe"
     } else {
@@ -20,13 +20,28 @@ frm.addEventListener("submit", (e) => {
         } else {
             erros.push(numero) // adiciona numero ao vetor
             const numErros = erros.length // obtém tamanho do vetor
-            const numChances = CHANCES - numErros // calcula número de chances
+            const numChances = CHANCES - numErros // calcula número de chances  
+
             // exibe o número de erros, conteúdo do vetor e nº de chances
             respErros.innerText = `${numErros} (${erros.join(", ")})`
             respChances.innerText = numChances
             if (numChances == 0) {
-                
+                alert("Suas chances acabaram...")
+                frm.btSubmit.disabled = true // troca status dos botões
+                frm.btNovo.className = "exibe"
+                respDica.innerText = `Game Over!! Número sorteado: ${sorteado}`
+
+            } else {
+                // usa operador ternário para mensagem de dica
+                const dica = numero < sorteado ? "maior" : "menor"
+                respDica.innerText = `Dica: O número sorteado é ${dica} do que ${numero}`
             }
         }
-    }
+    } 
+    frm.inNumero.value = "" //limpa campo de entrada
+    frm.inNumero.focus() // posiciona o cursor neste campo
+})
+
+frm.btNovo.addEventListener("click", () => {
+    location.reload() //recarrega a pagina 
 })
